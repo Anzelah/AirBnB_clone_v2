2b4f588501f12d41f7a07d7ebe0ac82544c22a18
Anzelah <jepkoechanzellah@gmail.com>
2023-10-04 11:25:56 -0700

    checking if script is correct first before running n server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Install nginx web server
+
+sudo apt-get update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/ && sudo touch /data/web_static/releases/test/index.html
+sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+
+sudo rm -rf /data/web_static/current #remove if exists
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current #ln -s <target> <current>
+sudo chown -R ubuntu: ubuntu /data/
+sudo sed -i '61i \\tlocation \/hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
