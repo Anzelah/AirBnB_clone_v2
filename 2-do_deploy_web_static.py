a33f5f1479d5b69dc7517c6a22223311ede6e325
Anzelah <jepkoechanzellah@gmail.com>
2023-10-05 12:44:31 -0700

    testing the scipt before running it in server

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines the modules"""
+
+import os
+
+def do_deploy(archive_path):
+    """Distribute an archive to your web servers"""
+
+    env.hosts = [
+            '54.210.88.27 web-01',
+            '52.91.117.155 web-02'
+            ]
+
+    if not os.path.exists("archive_path"):
+        return False
+
+    uncompressed = "/data/web_static/releases/archive_path"
+    put("archive_path, /tmp/")
+    with cd("/tmp"):
+        run("gunzip -c {} > {}" .format(archive_path, uncompressed))
+        sudo("rm {}".format(archive_path))
+    sudo("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/archive_path /data/web_static/current")
+
+    if (do.deploy().succeeded):
+        return True
+    else:
+        return 
+    False
