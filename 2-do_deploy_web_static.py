4c722e378c870d7475fa1a67816f89a09e8465da
Anzelah <jepkoechanzellah@gmail.com>
2023-10-05 15:22:53 -0700

    still debugging

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,8 +6,8 @@ from fabric.api import *
 
 
 env.hosts = [
-            '54.210.88.27 web-01',
-            '52.91.117.155 web-02'
+            '54.210.88.27',
+            '52.91.117.155'
             ]
 
 def do_deploy(archive_path):
@@ -15,14 +15,19 @@ def do_deploy(archive_path):
 
     if not os.path.exists(archive_path):
         return False
-
-    archive_filename = os.path.basename(archive_path)
-    uncompressed = "/data/web_static/releases/archive_filename"
+    
+    filename = archive_path.split('/')[-1]
+    archive_filename = os.path.splitext(archive_path)[0]
+    uncompressed = "/data/web_static/releases/{}/" .format(archive_filename)
+    fname_noext = os.path.splitext(filename)[0]
 
     put(archive_path, "/tmp/")
-    with cd("/tmp"):
-        run("tar -xzf {} -C {}" .format(archive_path, uncompressed))
-        run("rm {}".format(archive_path))
+    run("mkdir -p /data/web_static/releases/{}/" .format(fname_noext))
+#    with cd("/tmp"):
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/" .format(filename, fname_noext))
+    run("rm /tmp/{}".format(filename))
+    run("mv data/web_static/releases/{}/web_static/* data/web_static/releases/{}/" .format(fname_noext, fname_noext))
+    run("rm -rf {}web_static" .format(filename))
     run("rm -rf /data/web_static/current")
     run("ln -s /data/web_static/releases/{} /data/web_static/current" .format(archive_filename))
     results = run("ls {}" .format(uncompressed)).succeeded #check if deployment was succesful
