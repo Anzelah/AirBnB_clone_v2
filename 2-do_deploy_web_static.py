26a90cb5245ecd3da7ebe9fb201becba3260c6ab
Anzelah <jepkoechanzellah@gmail.com>
2023-10-05 13:22:58 -0700

    move env.hosts to outside function to properly set it

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,27 +4,28 @@
 import os
 from fabric.api import *
 
-def do_deploy(archive_path):
-    """Distribute an archive to your web servers"""
 
     env.hosts = [
             '54.210.88.27 web-01',
             '52.91.117.155 web-02'
             ]
 
-    if not os.path.exists("archive_path"):
+def do_deploy(archive_path):
+    """Distribute an archive to your web servers"""
+
+    if not os.path.exists(archive_path):
         return False
 
     uncompressed = "/data/web_static/releases/archive_path"
-    put("archive_path", "/tmp/")
+    put(archive_path, "/tmp/")
     with cd("/tmp"):
         run("tar -xzf {} -C {}" .format(archive_path, uncompressed))
-        run("rm {}".format(archive_path))
+    run("rm {}".format(archive_path))
     run("rm -rf /data/web_static/current")
     run("ln -s /data/web_static/releases/archive_path /data/web_static/current")
+    results = run("ls {}" .format(uncompressed)).succeeded #check if deployment was succesful
 
-    if (do_deploy().succeeded):
+    if results:
         return True
     else:
-        return 
-    False
+        return False
