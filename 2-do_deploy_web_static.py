0983601f4bbcf81a7c86c0e992b701fa1cf33d1e
Anzelah <jepkoechanzellah@gmail.com>
2023-10-06 12:23:34 -0700

    pycodestyle update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,6 +10,7 @@ env.hosts = [
             '52.91.117.155'
             ]
 
+
 def do_deploy(archive_path):
     """Distribute an archive to your web servers"""
 
@@ -22,7 +23,7 @@ def do_deploy(archive_path):
     extracted = extract_folderpath + archive_noext
 
     put(archive_path, "/tmp/")
-    run("mkdir -p {}" .format(extracted)) #mkdir to extract files to if not present
+    run("mkdir -p {}" .format(extracted))  # extract files to if not present
     run("tar -xzf /tmp/{} -C {}/" .format(archive, extracted))
     run("rm /tmp/{}" .format(archive))
     run("mv {}/web_static/* {}" .format(extracted, extracted))
@@ -30,8 +31,4 @@ def do_deploy(archive_path):
     run("rm -rf /data/web_static/current")
     run("ln -s {} /data/web_static/current" .format(extracted))
 
-    results = run("ls {}" .format(extracted))
-    if results.succeeded:
-        return True
-    else:
-        return False
+    return True
